51620035b04295455c22d3ed2596321012afa941
ZootHii <ahmet-yildirim_5858@outlook.com>
2022-12-04 15:55:07 +0300

    error handling with feign error decoder so i can change the returning message based on my own directions

--- library-service/src/main/java/com/libon/libraryservice/exception/BookNotFoundException.java
+++ library-service/src/main/java/com/libon/libraryservice/exception/BookNotFoundException.java
@@ -1,8 +1,25 @@
 package com.libon.libraryservice.exception;
 
+import com.libon.libraryservice.client.ExceptionMessage;
+
 public class BookNotFoundException extends RuntimeException {
 
-    public BookNotFoundException(String s) {
-        super(s);
+    private ExceptionMessage exceptionMessage;
+
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+
+    public BookNotFoundException(ExceptionMessage exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    public BookNotFoundException(String message, ExceptionMessage exceptionMessage) {
+        super(message);
+        this.exceptionMessage = exceptionMessage;
+    }
+
+    public ExceptionMessage getExceptionMessage() {
+        return exceptionMessage;
     }
 }
