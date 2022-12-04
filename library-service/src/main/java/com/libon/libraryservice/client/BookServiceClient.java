163174e44b84cd5a45f5e1077c4a5c3dc43d32ab
ZootHii <ahmet-yildirim_5858@outlook.com>
2022-12-04 19:49:01 +0300

    fault tolerance by using fallback, this could be used for creating a default value changing the request path creating another process this way when a microservice cant be available the whole system stays reliable Hystreix deprecated used resilience4j https://www.javatpoint.com/fault-tolerance-with-hystrix#:~:text=This%20aspect%20of%20a%20microservice,detected%2C%20the%20circuit%20breaker%20opens.

--- library-service/src/main/java/com/libon/libraryservice/client/BookServiceClient.java
+++ library-service/src/main/java/com/libon/libraryservice/client/BookServiceClient.java
@@ -2,6 +2,9 @@ package com.libon.libraryservice.client;
 
 import com.libon.libraryservice.dto.BookDto;
 import com.libon.libraryservice.dto.BookIdDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +13,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "book-service", path = "/v1/books")
 public interface BookServiceClient {
 
-    @GetMapping("/isbn/{isbn}")
+    Logger logger = LoggerFactory.getLogger(BookServiceClient.class);
+
+    @GetMapping("/isbn/{isbn}") // this will work with circuitBreaker before errorDecoder
+    @CircuitBreaker(name = "getByIsbnCircuitBreaker", fallbackMethod = "getByIsbnFallback")
     ResponseEntity<BookIdDto> getByIsbn(@PathVariable String isbn);
 
+    default ResponseEntity<BookIdDto> getByIsbnFallback(String isbn, Exception exception) {
+        logger.info("Book not found by isbn: " + isbn + ", returning default BookDto object.");
+        return ResponseEntity.ok(new BookIdDto("default-id", "default-isbn"));
+    }
+
+//    @GetMapping("/id/{id}") // this will work with error decoder there is no fallback/circuitBreaker
+//    ResponseEntity<BookDto> getById(@PathVariable String id);
+
     @GetMapping("/id/{id}")
+    @CircuitBreaker(name = "getByIdCircuitBreaker", fallbackMethod = "getByIdFallback")
     ResponseEntity<BookDto> getById(@PathVariable String id);
+
+    default ResponseEntity<BookDto> getByIdFallback(String isbn, Exception exception) {
+        logger.info("Book not found by id: " + isbn + ", returning default BookDto object.");
+        return ResponseEntity.ok(new BookDto(new BookIdDto("default-id", "default-isbn")));
+    }
 }
